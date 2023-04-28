1f47a74f4b10d151e8e010bd69e5191082303da9
Ali Mashayekhi <alim201381@gmail.com>
2023-04-28 12:46:09 +0430

    add vazir font

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,18 +1,25 @@
 import {Link, Outlet} from "react-router-dom";
+import {ThemeProvider} from "@mui/material";
+import {theme} from '../theme/rtl-theme';
+import RTL from "../theme/RTL";
 
 export default function Root() {
     return (
-        <>
-            <div id="sidebar">
-                <h1>React Router Contacts</h1>
-                <p><Link to={'/login'}>login</Link></p>
+        <ThemeProvider theme={theme}>
+            <RTL>
+                <div id="sidebar">
+                    سلام این یک متن فارسی است
+                    <h1>React Router Contacts</h1>
+                    <p><Link to={'/login'}>login</Link></p>
+                    <p><Link to={'/sing-up'}>sing up</Link></p>
+                    <p><Link to={'/leave-requests'}>leave requests</Link></p>
 
-                <p><Link to={'/sing-up'}>sing up</Link></p>
 
-            </div>
-            <Outlet>
-                
-            </Outlet>
-        </>
+                </div>
+                <Outlet>
+
+                </Outlet>
+            </RTL>
+        </ThemeProvider>
     );
 }
