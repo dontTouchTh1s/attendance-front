bfde69e9caf9aa17d3e88ad03e4655dfbf003e97
Ali Mashayekhi <alim201381@gmail.com>
2023-05-16 15:46:58 +0330

    Send post request to create new request with axios

--- src/Pages/CreateRquest/CreateRequest.js
+++ src/Pages/CreateRquest/CreateRequest.js
@@ -12,19 +12,26 @@ import {
     Typography
 } from "@mui/material";
 import Api from "../../Api";
-import {LocalizationProvider} from '@mui/x-date-pickers-pro';
+import {LocalizationProvider, TimePicker} from '@mui/x-date-pickers-pro';
 import {DateRangePicker} from '@mui/x-date-pickers-pro/DateRangePicker';
 import {AdapterMomentJalaali} from '@mui/x-date-pickers-pro/AdapterMomentJalaali';
 import dayjs from "dayjs";
-
+import './create-request.css';
+import {DatePicker} from "@mui/x-date-pickers-pro";
+import moment from "moment-jalaali";
 
 function CreateLeaveRequest() {
-    const [type, setType] = useState(0);
+    const [type, setType] = useState('');
     const [dateRange, setDateRange] = useState([
-        dayjs('2022-04-17'),
-        dayjs('2022-04-21'),
+        moment(),
+        moment().add(1, 'days'),
     ]);
-    const [description, setDescription] = useState();
+    //
+    const [fromHour, setFromHour] = useState(dayjs('2022-04-17T15:30'));
+    const [toHour, setToHour] = useState(dayjs('2022-04-17T15:30'));
+    const [duration, setDuration] = useState('');
+    const [description, setDescription] = useState('');
+    const [leaveTimingType, setLeaveTimingType] = useState('');
 
 
     async function handleSubmit(e) {
@@ -32,6 +39,18 @@ function CreateLeaveRequest() {
 
 
         let formData = new FormData();
+        formData.append('type', 'leave');
+        formData.append('leave_type', type);
+        formData.append('dates', JSON.stringify(dateRange));
+        formData.append('description', description);
+        if (leaveTimingType === 1) {
+            formData.append('from_hour', fromHour);
+            formData.append('to_hour', fromHour);
+        }
+        for (const value of formData.values())
+            console.log(value);
+
+
         try {
             const response = await Api.post('/requests/create', formData);
             // handle successful response
@@ -49,6 +68,14 @@ function CreateLeaveRequest() {
             }
         }
 
+    }
+
+    let hourPickerDisplay = leaveTimingType === 'hourly' ? '' : 'hidden';
+    let dateRangePicker = leaveTimingType === 'daily' ? '' : 'hidden';
+
+    function handleDateRangeChange(dateRage) {
+        dateRage.map(date => date.getDay);
+        setDateRange(dateRage)
     }
 
     return (
@@ -83,17 +110,69 @@ function CreateLeaveRequest() {
                                     label="نوع مرخصی"
                                     autoFocus
                                 >
-                                    <MenuItem value={0}>استحقاقی</MenuItem>
-                                    <MenuItem value={1}>استعلاحی</MenuItem>
-                                    <MenuItem value={2}>بدون حقوق</MenuItem>
+                                    <MenuItem value={'paid'}>استحقاقی</MenuItem>
+                                    <MenuItem value={'sick'}>استعلاجی</MenuItem>
+                                    <MenuItem value={'noPaid'}>بدون حقوق</MenuItem>
+                                    <MenuItem value={'optional'}>انتخابی</MenuItem>
                                 </Select>
                             </FormControl>
 
+                            <FormControl
+                                sx={{
+                                    width: '100%',
+                                }}>
+                                <InputLabel id="type-label">نوع خروج</InputLabel>
+                                <Select
+                                    labelId="type-label"
+                                    id="type"
+                                    value={leaveTimingType}
+                                    onChange={(e) => setLeaveTimingType(e.target.value)}
+                                    label="نوع مرخصی"
+                                    autoFocus
+                                >
+                                    <MenuItem value={'daily'}>روزانه</MenuItem>
+                                    <MenuItem value={'hourly'}>ساعتی</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControl className={hourPickerDisplay + " form-control"}
+                                         sx={{
+                                             display: 'flex',
+                                             gap: '12px'
+                                         }}>
+                                <DatePicker
+                                    value={dateRange[0]}
+                                    onChange={(newValue) => setDateRange([newValue, newValue])}
+                                    fullWidth
+                                    label="تاریخ"
+                                    name="date"
+                                    id="date">
+                                </DatePicker>
+                                <TimePicker
+                                    label="از ساعت"
+                                    value={fromHour}
+                                    onChange={(newValue) => setFromHour(newValue)}
+                                />
+                                <TimePicker
+                                    label="تا ساعت"
+                                    value={toHour}
+                                    onChange={(newValue) => setToHour(newValue)}
+                                />
+                            </FormControl>
                             <DateRangePicker
+                                className={dateRangePicker + " form-control"}
                                 localeText={{start: 'از تاریخ', end: 'تا تاریخ'}}
                                 value={dateRange}
-                                onChange={(newValue) => setDateRange(newValue)}
+                                onChange={handleDateRangeChange}
                             />
+
+                            <TextField
+                                id="description"
+                                label="توضیحات"
+                                multiline
+                                value={description}
+                                onChange={newValue => setDescription(newValue.target.value)}
+                            />
+
                         </Box>
                         <Button
                             type="submit"
