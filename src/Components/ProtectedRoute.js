9b4cd3e1253deec3aa2270769bed330f0c7bb0f3
Ali Mashayekhi <alim201381@gmail.com>
2023-07-25 15:57:56 +0330

    protected route now check user for authenticate from api

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -1,15 +1,34 @@
 import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
+import Api from "../Api";
 
-function ProtectedRoute({children}){
+function ProtectedRoute({children}) {
     const navigate = useNavigate();
     useEffect(() => {
-        const user = localStorage.getItem('email');
-        if (user === null){
-            navigate('/login');
+        async function getUser() {
+            try {
+                await Api.get('/auth/');
+
+            } catch (error) {
+                if (error.response) {
+                    // handle error response
+                    if (error.response.status === 401)
+                        navigate('/login');
+                } else if (error.request) {
+                    // handle no response
+                    console.log(error.request);
+                } else {
+                    // handle other errors
+                    console.log('Error', error.message);
+                }
+            }
         }
+
+        getUser();
     });
+
 
     return children;
 }
+
 export default ProtectedRoute
