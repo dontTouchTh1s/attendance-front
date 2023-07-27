f3e7d9342972187c90723d4bd84697717a4a83ae
Ali Mashayekhi <alim201381@gmail.com>
2023-07-27 17:35:31 +0330

    check if user already authenticated, navigate to dashboard

--- src/Components/AlreadyLogin.js
+++ src/Components/AlreadyLogin.js
@@ -1,28 +1,31 @@
 import {useNavigate} from "react-router-dom";
-import {useContext, useLayoutEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import Api from "../Api";
 import CurrentPageContext from "./CurrentPageContext";
 
-function AlreadyLogin() {
+function AlreadyLogin({children}) {
     const navigate = useNavigate();
     const {setCurrentPage} = useContext(CurrentPageContext);
-
-    useLayoutEffect(() => {
+    const [checked, setChecked] = useState(false);
+    useEffect(() => {
         async function getUser() {
             try {
                 let response = await Api.get('/auth/');
                 if (response.status === 200) {
                     setCurrentPage(0);
-                    navigate('/dashboard');
+                    navigate('/Dashboard');
                 }
-
+                setChecked(true);
             } catch (error) {
-
+                setChecked(true);
             }
         }
 
         getUser();
     });
+    return (
+        checked ? children : ''
+    )
 }
 
 export default AlreadyLogin;
