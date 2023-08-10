ed1a7402f3437c5ad80e423788149e0d03645ff6
Ali Mashayekhi <alim201381@gmail.com>
2023-08-10 13:43:02 +0330

    last

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Box, Typography} from "@mui/material";
+import {BarChart} from "@mui/x-charts";
+import Api from "../../Api";
 
 function Dashboard() {
+    const [months, setMonths] = useState([]);
+    const [counts, setCounts] = useState([]);
+
+    async function fetchLeaveRequests() {
+        try {
+            const response = await Api.get('/leave-requests');
+            console.log(response)
+            setMonths(response.data.map((l) =>
+                l.month
+            ));
+            setCounts(response.data.map((l) =>
+                l.time
+            ));
+            // handle successful response
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    useEffect(() => {
+        fetchLeaveRequests();
+    }, [])
     return (
-        <>
-        </>
+        <Box>
+            <Typography component="h1" variant="h4">
+                داشبورد
+            </Typography>
+            {
+                counts.length !== 0 ?
+                    <BarChart
+                        xAxis={[{scaleType: 'band', data: months}]}
+                        series={[{data: counts, label: 'مرخصی ها در ماه های گذشته', type: 'bar'}]}
+                        width={500}
+                        height={300}
+                    /> : 'داده ای وجود ندارد'
+            }
+        </Box>
     )
 }
 
