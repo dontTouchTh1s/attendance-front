20569e6397e1be0ef2bce7629af5be465f0385e5
Ali Mashayekhi <alim201381@gmail.com>
2023-10-31 13:13:16 +0330

    Now check xsrf token before sending any request, and refresh token if its gone invalid

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,26 @@
 import axios from "axios";
+import getCookie from "./Functions/GetCookie/GetCookie";
 
 const instance = axios.create({
     baseURL: 'http://localhost:8000/api',
     withCredentials: true
-})
+});
+instance.interceptors.request.use(async (config) => {
+    if (!getCookie('XSRF-TOKEN')) {
+        await axios.get('http://localhost:8000/sanctum/csrf-cookie', {withCredentials: true});
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+instance.interceptors.response.use(response => response,
+    async (error) => {
+        if (error.response) {
+            if (error.response.status === 419) {
+                await axios.get('http://localhost:8000/sanctum/csrf-cookie', {withCredentials: true});
+                return axios(error.config);
+            }
+        }
+        return Promise.reject(error);
+    });
 export default instance
